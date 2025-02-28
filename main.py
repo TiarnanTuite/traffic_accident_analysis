3482cf7697b8d01d9c5a82f335155fdfe40ac1cb
Tiarnan Tuite <tuitetiarnan@gmail.com>
2025-02-28 13:52:26 +0000

    data cleaning documentation complete

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 # Loading cleaned dataset
 df = pd.read_csv(
-    "C:/Users/tuite/Desktop/Software Portfolio/python/Traffic_Accident_Analysis/data/cleaned_traffic/accidents.csv"
+    "C:/Users/tuite/Desktop/Software Portfolio/python/Traffic_Accident_Analysis/data/final_cleaned_accident_data.csv"
 )
 
 # Checking the first few rows to inspect the dataset

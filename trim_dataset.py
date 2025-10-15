9443aac8f9009120b516cedcc088e878f40d771e
Tiarnan Tuite <tuitetiarnan@gmail.com>
2025-10-15 12:05:01 +0100

    spelling fixes

--- trim_dataset.py
+++ trim_dataset.py
@@ -1,15 +1,14 @@
 import pandas as pd
 import os
 
-# Path to your big dataset
+# Path to main cleaned dataset
 input_path = os.path.join("data", "final_cleaned_accident_data.csv")
 
 # Load full dataset
-print("Loading full dataset...")
+print("Loading full dataset")
 df = pd.read_csv(input_path)
 print(f"Full dataset shape: {df.shape}")
-con
-# Take a random sample of 10,000 rows (adjust as needed)
+# Take a random sample of 10,000 rows
 sample_size = 10000
 df_sample = df.sample(sample_size, random_state=42)
 
